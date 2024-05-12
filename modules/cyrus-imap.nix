b088f346cf9a1df03e0858335bb04c4d867450b8
Moraxyc <i@qaq.li>
2024-05-12 13:45:16 +0800

    perf(modules): update cyrus

--- modules/cyrus-imap.nix
+++ modules/cyrus-imap.nix
@@ -31,118 +31,6 @@ let
     path
     ;
 
-  cyrusOptions =
-    { ... }:
-    {
-      options = {
-        cmd = mkOption {
-          type = listOf str;
-          description = "Command (with arguments) to spawn as a child process.";
-          example = literalExpression ''
-            [ "imapd" "-s" ]
-          '';
-        };
-        babysit = mkOption {
-          type = nullOr int;
-          default = null;
-          description = "If this is non-zero, at least one process will always be pre-forked. Otherwise, the max fork rate will be set to 10.";
-          example = 0;
-        };
-        listen = mkOption {
-          type = nullOr str;
-          default = null;
-          description = ''
-            The address or socket path to listen on.
-          '';
-          example = "/run/cyrus/lmtp";
-        };
-        proto = mkOption {
-          type = nullOr (enum [
-            "tcp"
-            "tcp4"
-            "tcp6"
-            "udp"
-            "udp4"
-            "udp6"
-          ]);
-          default = null;
-          description = ''
-            Protocol used for Cyrus.
-            - `tcp`: Attempt to use TCP with both IPv4 and IPv6 if possible.
-            - `udp`: Attempt to use UDP with both IPv4 and IPv6 if possible.
-            - `tcp4`: Similar to `tcp` but only uses IPv4.
-            - `tcp6`: Similar to `tcp` but only uses IPv6.
-            - `udp4`: Similar to `udp` but only uses IPv4.
-            - `udp6`: Similar to `udp` but only uses IPv6.
-          '';
-          example = "tcp";
-        };
-        prefork = mkOption {
-          type = nullOr int;
-          default = null;
-          description = ''
-            Amount of instances to always have running and waiting for a connection. This can be used to achieve, for example, faster initial response time.
-            If you are listening on multiple network types (i.e. IPv4/IPv6) then a process will be forked for each address, creating twice as many processes as you might expect.
-          '';
-          example = 0;
-        };
-        maxchild = mkOption {
-          type = nullOr int;
-          default = null;
-          description = ''
-            Maximum amount of instances to spawn. A value of -1 means unlimited.
-          '';
-          example = -1;
-        };
-        maxfds = mkOption {
-          type = nullOr int;
-          default = null;
-          description = ''
-            Maximum number of file descriptors to limit Cyrus to.
-          '';
-          example = 256;
-        };
-        maxforkrate = mkOption {
-          type = nullOr int;
-          default = null;
-          description = ''
-            Maximum number of processes to fork per second; the master will ensure it doesn't fork faster than this.
-          '';
-          example = 0;
-        };
-        period = mkOption {
-          type = nullOr int;
-          default = null;
-          description = ''
-            The interval (in minutes) at which to run the command defined in . This integer value is optional, but SHOULD be a positive integer > 10.
-          '';
-          example = 0;
-        };
-        at = mkOption {
-          type = nullOr int;
-          default = null;
-          description = ''
-            The time (24-hour format) at which to run the command each day. If set to a valid time (0000-2359), period is automatically set to 1440. This string argument is optional.
-          '';
-          example = 0;
-        };
-        wait = mkOption {
-          type = nullOr str;
-          default = null;
-          description = ''
-            Switch: whether or not master(8) should wait for this daemon to successfully start before continuing to load.
-            If wait=n (the default), the daemon will be started asynchronously along with the service processes. The daemon process will not have file descriptor 3 open, and does not need to indicate its readiness.
-            If wait=y, the daemon MUST write "ok\r\n" to file descriptor 3 to indicate its readiness; if it does not do this, and master has been told to wait, master will continue to wait.... If it writes anything else to this descriptor, or closes it before writing "ok\r\n", master will exit with an error.
-            Daemons with wait=y will be started sequentially in the order they are listed in cyrus.conf, waiting for each to report readiness before the next is started.
-            Service processes, and wait=n daemons, are not started until after the wait=y daemons are all started and ready.
-            At shutdown, wait=y daemons will be terminated sequentially in the reverse order they were started, commencing after all other services and wait=n daemons have finished.
-            If a daemon that was started with wait=y exits unexpectedly, such that master restarts it, master will restart it asynchronously, without waiting for it to report its readiness. In this case, file descriptor 3 will not be open and the daemon should not try to write to it.
-            If master is told to reread its config with a SIGHUP, this signal will be passed on to wait=y daemons like any other service. If the daemon exits in response to the signal, master will restart it asynchronously, without waiting for it to report its readiness. In this case too, file descriptor 3 will not be open and the daemon should not try to write to it.
-          '';
-          example = "0";
-        };
-      };
-    };
   mkCyrusConfig =
     settings:
     concatStringsSep "\n  " (
@@ -229,7 +117,6 @@ in
                 ];
               };
             };
-            type = attrsOf (submodule cyrusOptions);
             description = ''
               This section lists the processes to run before any SERVICES are spawned. This section is typically used to initialize databases. Master itself will not startup until all tasks in START have completed, so put no blocking commands here.
             '';
@@ -258,7 +145,6 @@ in
                 prefork = 0;
               };
             };
-            type = attrsOf (submodule cyrusOptions);
             description = ''
               This section is the heart of the cyrus.conf file. It lists the processes that should be spawned to handle client connections made on certain Internet/UNIX sockets.
             '';
@@ -305,14 +191,12 @@ in
                 period = 30;
               };
             };
-            type = attrsOf (submodule cyrusOptions);
             description = ''
               This section lists processes that should be run at specific intervals, similar to cron jobs. This section is typically used to perform scheduled cleanup/maintenance.
             '';
           };
           DAEMON = mkOption {
             default = { };
-            type = attrsOf (submodule cyrusOptions);
             description = ''
               This section lists long running daemons to start before any SERVICES are spawned. master(8) will ensure that these processes are running, restarting any process which dies or forks. All listed processes will be shutdown when master(8) is exiting.
             '';
@@ -330,14 +214,6 @@ in
           (listOf str)
         ]);
         options = {
-          admins = mkOption {
-            type = listOf str;
-            default = [ "cyrus" ];
-            description = ''
-              The list or string of userids with administrative rights.
-              Note that accounts used by users should not be administrators. Administrative accounts should not receive mail. That is, if user "jbRo" is a user reading mail, he should not also be in the admins line. Some problems may occur otherwise, most notably the ability of administrators to create top-level mailboxes visible to users, but not writable by users.
-            '';
-          };
           configdirectory = mkOption {
             type = path;
             default = "/var/lib/cyrus";
@@ -345,162 +221,55 @@ in
               The pathname of the IMAP configuration directory. This field is required.
             '';
           };
-          proc_path = mkOption {
+          lmtpsocket = mkOption {
             type = path;
-            default = "/run/cyrus/proc";
+            default = "/run/cyrus/lmtp";
             description = ''
-              Path to proc directory. Default is NULL - must be an absolute path if specified.
+              Unix socket that lmtpd listens on, used by deliver(8). This should match the path specified in cyrus.conf(5).
             '';
           };
-          tls_sessions_db_path = mkOption {
+          idlesocket = mkOption {
             type = path;
-            default = "/run/cyrus/db/tls_sessions.db";
+            default = "/run/cyrus/idle";
             description = ''
-              The absolute path to the TLS sessions db file.
+              Unix socket that idled listens on.
             '';
           };
-          statuscache_db_path = mkOption {
+          notifysocket = mkOption {
             type = path;
-            default = "/run/cyrus/db/statuscache.db";
+            default = "/run/cyrus/notify";
             description = ''
-              The absolute path to the statuscache db file.
-            '';
-          };
-          ptscache_db_path = mkOption {
-            type = path;
-            default = "/run/cyrus/db/ptscache.db";
-            description = ''
-              The absolute path to the ptscache db file.
-            '';
-          };
-          duplicate_db_path = mkOption {
-            type = path;
-            default = "/run/cyrus/db/deliver.db";
-            description = ''
-              The absolute path to the duplicate db file.
-            '';
-          };
-          mboxname_lockpath = mkOption {
-            type = path;
-            default = "/run/cyrus/lock";
-            description = ''
-              Path to mailbox name lock files
-            '';
-          };
-          tls_session_timeout = mkOption {
-            type = ints.between 0 1440;
-            default = 1440;
-            description = ''
-              The length of time (in minutes) that a TLS session will be cached for later reuse.
-              The maximum value is 1440 (24 hours), the default.
-              A value of 0 will disable session caching.
-            '';
-          };
-          sasl_auto_transition = mkOption {
-            type = bool;
-            default = true;
-            description = ''
-              If enabled, the SASL library will automatically create authentication secrets when given a plaintext password. See the SASL documentation.
-            '';
-          };
-          allowplaintext = mkOption {
-            type = bool;
-            default = true;
-            description = ''
-              Allow plaintext logins by default (SASL PLAIN)
-            '';
-          };
-          sievedir = mkOption {
-            type = path;
-            default = "/var/lib/cyrus/sieve";
-            description = ''
-              If sieveusehomedir is false, this directory is searched for Sieve scripts.
-            '';
-          };
-          partition-default = mkOption {
-            type = str;
-            default = "/var/lib/cyrus/storage";
-            description = ''
-              The pathname of the partition **default**.
-            '';
-          };
-          defaultpartition = mkOption {
-            type = str;
-            default = "default";
-            description = ''
-              The partition name used by default for new mailboxes. If not specified, the partition with the most free space will be used for new mailboxes.
-              Note that the partition specified by this option must also be specified as partition-name, where you substitute 'name' for the alphanumeric string you set defaultpartition to.
-            '';
-          };
-          popminpoll = mkOption {
-            type = int;
-            default = 1;
-            description = ''
-              Minimum time between POP mail fetches in minutes   The default domain for virtual domain support
-            '';
-          };
-          defaultdomain = mkOption {
-            type = str;
-            default = "localhost";
-            description = ''
-              The default domain for virtual domain support
-            '';
-          };
-          virtdomains = mkOption {
-            type = enum [
-              "off"
-              "userid"
-              "on"
-            ];
-            default = "on";
-            description = ''
-              off: Cyrus does not know or care about domains. Only the local part of email addresses is ever considered. This is not recommended for any deployment.
-
-              userid: The user's domain is determined by splitting a fully qualified userid at the last '@' or '%' symbol. If the userid is unqualified, the defaultdomain will be used. This is the recommended configuration for all deployments. If you wish to provide calendaring services you must use this configuration.
-
-              on: Fully qualified userids are respected, as per "userid". Unqualified userids will have their domain determined by doing a reverse lookup on the IP address of the incoming network interface, or if no record is found, the defaultdomain will be used.
-
-              Allowed values: off, userid, on
-            '';
-          };
-          hashimapspool = mkOption {
-            type = bool;
-            default = true;
-            description = ''
-              If enabled, the partitions will also be hashed, in addition to the hashing done on configuration directories.
-              This is recommended if one partition has a very bushy mailbox tree.
-            '';
-          };
-          httpmodules = mkOption {
-            type = listOf str;
-            example = [
-              "caldav"
-              "carddav"
-              "domainkey"
-              "ischedule, rss"
-            ];
-            default = [ "caldav" ];
-            description = ''
-              List of HTTP modules that will be enabled in httpd(8).
-              Allowed values: caldav carddav domainkey ischedule rss
-            '';
-          };
-          sasl_pwcheck_method = mkOption {
-            type = listOf str;
-            default = [ "pwcheck" ];
-            example = [
-              "saslauthd"
-              "auxprop"
-              "pwcheck"
-            ];
-            description = ''
-              The mechanism(s) used by the server to verify plaintext passwords.
-              Possible values are "saslauthd", "auxprop", "pwcheck" and "alwaystrue".  They are tried in order, you can specify more than one.
+              Unix domain socket that the mail notification daemon listens on.
             '';
           };
         };
       };
-      default = { };
+      default = {
+        admins = [ "cyrus" ];
+        allowplaintext = true;
+        defaultdomain = "localhost";
+        defaultpartition = "default";
+        duplicate_db_path = "/run/cyrus/db/deliver.db";
+        hashimapspool = true;
+        httpmodules = [
+          "carddav"
+          "caldav"
+        ];
+        mboxname_lockpath = "/run/cyrus/lock";
+        partition-default = "/var/lib/cyrus/storage";
+        popminpoll = 1;
+        proc_path = "/run/cyrus/proc";
+        ptscache_db_path = "/run/cyrus/db/ptscache.db";
+        sasl_auto_transition = true;
+        sasl_pwcheck_method = [ "saslauthd" ];
+        sievedir = "/var/lib/cyrus/sieve";
+        statuscache_db_path = "/run/cyrus/db/statuscache.db";
+        syslog_prefix = "cyrus";
+        tls_client_ca_dir = "/etc/ssl/certs";
+        tls_session_timeout = 1440;
+        tls_sessions_db_path = "/run/cyrus/db/tls_sessions.db";
+        virtdomains = "on";
+      };
       description = "IMAP configuration settings. [imapd.conf(5)](https://www.cyrusimap.org/imap/reference/manpages/configs/imapd.conf.html)";
     };
 
@@ -550,19 +319,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.cyrus-imap = {
-      imapdSettings = {
-        syslog_prefix = "cyrus";
-        lmtpsocket = "/run/cyrus/lmtp";
-        idlesocket = "/run/cyrus/idle";
-        notifysocket = "/run/cyrus/notify";
-        tls_server_cert = mkIf (cfg.sslServerCert != null) cfg.sslServerCert;
-        tls_server_key = mkIf (cfg.sslServerKey != null) cfg.sslServerKey;
-        tls_client_ca_file = mkIf (cfg.sslCACert != null) cfg.sslCACert;
-        tls_client_ca_dir = "/etc/ssl/certs";
-      };
-    };
-
     users.users.cyrus = optionalAttrs (cfg.user == "cyrus") {
       description = "Cyrus IMAP user";
       isSystemUser = true;
