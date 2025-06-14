f407c2b4377b1bc1698a3fc80bd5b790df7bfcc5
Moraxyc <i@qaq.li>
2025-06-14 17:17:10 +0800

    rime-ice: drop

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,19 +1,14 @@
 {
   pkgs ? import <nixpkgs> { },
   lib ? pkgs.lib,
-  pkgs-stable ? pkgs,
   pkgs-cuda ? pkgs,
   sources ? pkgs.callPackage ../_sources/generated.nix { },
   inputs' ? null,
-  inputs ? null,
-  system ? builtins.currentSystem,
   ...
 }:
 
 let
-  call = p: pkgs.lib.callPackageWith (pkgs // { inherit sources; }) p { };
   call-cuda = p: pkgs.lib.callPackageWith (pkgs-cuda // { inherit sources; }) p { };
-  call-stable = p: pkgs.lib.callPackageWith (pkgs-stable // { inherit sources; }) p { };
 in
 {
   # Cache
@@ -23,6 +18,7 @@ in
   self-linux-enable-ir-emitter = call-cuda ./by-name/li/linux-enable-ir-emitter/package.nix;
 
   hath-rust = throw "hath-rust has been merged into nixpkgs/nixos-unstable";
+  rime-ice = throw "rime-ice has been merged into nixpkgs/nixos-unstable";
 }
 // (lib.mapAttrs' (
   dir: _:
