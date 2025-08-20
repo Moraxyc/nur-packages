3ef5793e8682a8d2b8e587c5b1eae45d0197f414
Moraxyc <i@qaq.li>
2025-08-20 22:35:06 +0800

    self-libinput: fix

--- pkgs/self/libinput/package.nix
+++ pkgs/self/libinput/package.nix
@@ -2,7 +2,6 @@
 pkgs.libinput.overrideAttrs (
   _finalAttrs: _prevAttrs: {
     patches = _prevAttrs.patches or [ ] ++ [
-      ./0001-gestures-fix-acceleration-in-3fg-drag.patch
       ./0002-enable-3fg-drag-by-default.patch
     ];
   }
