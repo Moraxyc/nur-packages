a2080c4259f63b4e260b06027785d5b07d5ecc69
Moraxyc <i@qaq.li>
2026-02-07 15:04:18 +0800

    self-niri: use nvfetcher to get cargo.lock

--- pkgs/by-name/se/self-niri/package.nix
+++ pkgs/by-name/se/self-niri/package.nix
@@ -2,31 +2,36 @@
   inputs,
   stdenv,
   runCommand,
+  sources,
   rustPlatform,
 }:
 let
-  package = inputs.niri.packages.${stdenv.hostPlatform.system} or null;
+  package =
+    inputs.niri.packages.${stdenv.hostPlatform.system} or inputs.niri.packages.x86_64-linux or null;
 in
 if package != null then
   package.default.overrideAttrs (
     finalAttrs: prevAttrs: {
-      src = runCommand "${finalAttrs.pname}-patched-src" { } ''
-        cp -r ${prevAttrs.src} $out
-        pushd $out
+      src = runCommand "${finalAttrs.pname}-patched-src.tar.gz" { } ''
+        cp -r ${prevAttrs.src} source
+        pushd source
           chmod -R +w .
           patch -p1 < ${./0001-input-Change-swipe-gesture-to-match-four-finger-usag.patch}
           patch -p1 < ${./remote-desktop.patch}
         popd
+
+        tar -czf $out source
+      '';
+      unpackPhase = ''
+        runHook preUnpack
+        tar -xzf $src --strip-components=1
+        runHook postUnpack
       '';
       buildFeatures = prevAttrs.buildFeatures ++ [ "xdp-gnome-remote-desktop" ];
       checkFeatures = finalAttrs.buildFeatures;
       cargoBuildFeatures = prevAttrs.cargoBuildFeatures ++ [ "xdp-gnome-remote-desktop" ];
       cargoCheckFeatures = finalAttrs.checkFeatures;
-      cargoDeps = rustPlatform.fetchCargoVendor {
-        inherit (finalAttrs) src;
-        # nix-update auto
-        hash = "sha256-aouPvpdxgmODvD8S/0moMde76XbfNuM1paQ21JsK0Pc=";
-      };
+      cargoDeps = rustPlatform.importCargoLock sources.niri.cargoLock."Cargo.lock";
       passthru = (prevAttrs.passthru or { }) // {
         _ignoreOverride = true;
       };
