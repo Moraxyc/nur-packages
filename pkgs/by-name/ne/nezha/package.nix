1dcece6195e8705bae6bd1ce6497c28da84330be
Moraxyc <i@qaq.li>
2026-06-22 11:13:54 +0800

    nezha-*: init

---
+++ pkgs/by-name/ne/nezha/package.nix
@@ -0,0 +1,18 @@
+{
+  nixpkgs,
+  sources,
+
+  source ? sources.nezha,
+}:
+nixpkgs.nezha.overrideAttrs (
+  finalAttrs: prevAttrs: {
+    inherit (source) version src;
+
+    # nix-update auto
+    vendorHash = "sha256-rYzkaJqk5r31Uagn1FRFDeICUeK392o1fyP6IBk9zgk=";
+
+    passthru = (prevAttrs.passthru or { }) // {
+      _ignoreOverride = true;
+    };
+  }
+)
